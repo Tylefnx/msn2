a55a2fb6a96ff3e3d2f1da2f26a7ca96781b3ff2
tylefnx <tayfunucuncu@gmail.com>
2024-11-22 17:09:59 +0300

    asdsad

---
+++ lib/authentication/infrastructure/authentication_repository.dart
@@ -0,0 +1,7 @@
+import 'package:msn2/authentication/infrastructure/authentication_service.dart';
+
+class AuthenticationRepository {
+  final AuthenticationService _service;
+
+  const AuthenticationRepository(this._service);
+}
--- lib/authentication/infrastructure/authentication_service.dart
+++ lib/authentication/infrastructure/authentication_service.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 class AuthenticationService {
   final Dio _dio;
 
-  AuthenticationService(this._dio);
+  const AuthenticationService(this._dio);
 
   Future<Response> login({
     required String username,
